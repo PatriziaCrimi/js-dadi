986801d4aa11edd61d05f8db698fe44406106fe7
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-14 20:39:48 +0200

    Impostato algoritmo base con output in console

--- action.js
+++ action.js
@@ -1,7 +1,17 @@
-/* INSTRUCTIONS
-Generare un numero casuale tra 1 e 6 per simulare il lancio di un dado (giocata 1).
-Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
-Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
-*/
+// Variables and constants initialization
+var random_number_pc = Math.floor(Math.random() * 6) + 1;
+var random_number_player1 = Math.floor(Math.random() * 6) + 1;
+var player1_name = prompt('Enter the name of player one.');
 
-// Variables and constants inizialization
+// ******************* Output in Console *******************
+console.log('The computer\'s random number is ' + random_number_pc + '.');
+console.log(player1_name +'\'s number is ' + random_number_player1 + '.');
+
+// ******************* Winner Check *******************
+if (random_number_pc > random_number_player1) {
+  console.log('The winner is the computer! It scored ' + random_number_pc + '.');
+} else if (random_number_pc < random_number_player1) {
+  console.log('The winner is ' + player1_name + '! He/she scored ' + random_number_player1 + '.');
+} else {
+  console.log('There are no winners. You both scored ' + random_number_pc + '!');
+}
