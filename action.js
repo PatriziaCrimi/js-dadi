72f788ed0f6a16db5f4d5c2735089c04f5964e97
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-14 14:24:55 +0200

    Impostati file

---
+++ action.js
@@ -0,0 +1,7 @@
+/* INSTRUCTIONS
+Generare un numero casuale tra 1 e 6 per simulare il lancio di un dado (giocata 1).
+Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
+Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
+*/
+
+// Variables and constants inizialization
