e2f8d992baa7a86781715bafb52c4f3496e3aab5
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-14 21:48:37 +0200

    Aggiunta interazione con l'utente e scelta numero di partite

--- action.js
+++ action.js
@@ -1,17 +1,43 @@
 // Variables and constants initialization
-var random_number_pc = Math.floor(Math.random() * 6) + 1;
-var random_number_player1 = Math.floor(Math.random() * 6) + 1;
-var player1_name = prompt('Enter the name of player one.');
+var player_name = prompt('Enter your name.');
+var number_of_games = parseInt(prompt('How many games do you want to play? Please enter a number.'));
 
-// ******************* Output in Console *******************
-console.log('The computer\'s random number is ' + random_number_pc + '.');
-console.log(player1_name +'\'s number is ' + random_number_player1 + '.');
+// ******************* Check data in Console *******************
+console.log('GAME INFO');
+console.log('Player one is ' + player_name + '.');
+console.log('The second player is the computer.');
+console.log('You are going to play ' + number_of_games + ' games.')
+console.log(' ');
 
-// ******************* Winner Check *******************
-if (random_number_pc > random_number_player1) {
-  console.log('The winner is the computer! It scored ' + random_number_pc + '.');
-} else if (random_number_pc < random_number_player1) {
-  console.log('The winner is ' + player1_name + '! He/she scored ' + random_number_player1 + '.');
-} else {
-  console.log('There are no winners. You both scored ' + random_number_pc + '!');
+// ******************* Game & Output in Console *******************
+console.log('SCORE');
+for (i = 0; i < number_of_games; i++) {
+  // Match counts
+  var match_number = i + 1;
+  console.log('*** Match number ' + match_number + ' ***.');
+  // Roll of the dice
+  var random_number_pc = Math.floor(Math.random() * 6) + 1;
+  var random_number_player = Math.floor(Math.random() * 6) + 1;
+  // ******** Winner Check ********
+  if (random_number_pc > random_number_player) {
+    // You lose
+    console.log('The winner is the computer! It scored ' + random_number_pc + '.');
+    console.log('You scored ' + random_number_player + '.');
+    console.log(' ');
+  } else if (random_number_pc < random_number_player) {
+    // You win
+    console.log('You won! ');
+    console.log(player_name + ' scored ' + random_number_player + '.');
+    console.log('The computer scored ' + random_number_pc + '.');
+    console.log(' ');
+  } else {
+    // No winners
+    console.log('There are no winners. You both scored ' + random_number_pc + '!');
+    console.log(' ');
+  }
 }
+/*
+// ******************* Play again *******************
+var play_again = prompt('Do you want to play again? Please enter YES or NO.');
+if ()
+*/
